2bbc0c23acdeac3ed3a564613599d455d5fc19dc
Colin.Mills <comills@brocksolutions.com>
2018-11-25 15:17:48 -0500

    Clean up gitignore

--- CameraService/CameraSharerer/MainActivity.cs
+++ CameraService/CameraSharerer/MainActivity.cs
@@ -1,15 +1,15 @@
-﻿
-using Android;
+﻿using Android;
 using Android.App;
 using Android.Content;
 using Android.Graphics;
 using Android.OS;
 using Android.Provider;
 using Android.Runtime;
-using Android.Support.V4.App;
 using Android.Support.V4.Content;
 using Android.Support.V7.App;
 using Android.Widget;
+
+using CameraSharerer.Managers;
 
 using AndroidFile = Java.IO.File;
 
@@ -18,21 +18,20 @@ namespace CameraSharerer
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private string _providerName = $"com.camerasharer.fileprovider";
+        // private const string IP = "192.168.0.111";
 
-        private static string[] PERMISSIONS =
+        private static string[] PERMISSIONS_NEEDED =
         {
             Manifest.Permission.Camera,
-            Manifest.Permission.WriteExternalStorage,
-            Manifest.Permission.ReadExternalStorage
+            Manifest.Permission.ReadExternalStorage,
+            Manifest.Permission.WriteExternalStorage
         };
-
-        private string _path;
-        //private string _providerName = $"{BuildConfig.ApplicationId}.fileprovider";
-        private string _providerName = $"test.fileprovider";
 
         private ImageView _imgView;
         private AndroidFile _file;
         private AndroidFile _directory;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -41,27 +40,36 @@ namespace CameraSharerer
             var btnTakeAPicture = FindViewById<Button>(Resource.Id.btnTakeAPicture);
             btnTakeAPicture.Click += BtnTakeAPicture_Click;
 
-            _imgView = FindViewById<ImageView>(Resource.Id.imgView);
-            _path = $"{Environment.GetExternalStoragePublicDirectory(Environment.DirectoryDcim)}";
+            var btnSendPicture = FindViewById<Button>(Resource.Id.btnSendPicture);
+            btnSendPicture.Click += BtnSendPicture_Click;
 
-            ActivityCompat.RequestPermissions(this, PERMISSIONS, 1);
+            _imgView = FindViewById<ImageView>(Resource.Id.imgView);
+
+            //@TODO(cjm): This should probably be done at app Startup, not in the main activity.
+            var permissionManager = new PermissionsManager(this, PERMISSIONS_NEEDED);
+            permissionManager.EnablePermissions();
+        }
+
+        private void BtnSendPicture_Click(object sender, System.EventArgs e)
+        {
         }
 
         private void BtnTakeAPicture_Click(object sender, System.EventArgs e)
         {
             Intent imageIntent = new Intent(MediaStore.ActionImageCapture);
 
+            //@TODO: Make this directory code better. Move it through etc.
             _directory = new AndroidFile(Environment.ExternalStorageDirectory, "cam-share-pics");
             if (!_directory.Exists())
             {
                 _directory.Mkdirs();
             }
 
-            _file = new AndroidFile(_directory, "test.png");
+            _file = new AndroidFile(_directory, UniqueImgFileName());
 
-            var uri = FileProvider.GetUriForFile(this, _providerName, _file);
-
-            imageIntent.PutExtra(MediaStore.ExtraOutput, uri);
+            // This file provider was a bit of a PITA. 
+            // Spent a couple days just trying to get it working.
+            imageIntent.PutExtra(MediaStore.ExtraOutput, FileProvider.GetUriForFile(this, _providerName, _file));
             imageIntent.SetFlags(ActivityFlags.GrantWriteUriPermission);
             StartActivityForResult(imageIntent, 0);
         }
@@ -70,13 +78,15 @@ namespace CameraSharerer
         {
             base.OnActivityResult(requestCode, resultCode, data);
 
-
             int height = Resources.DisplayMetrics.HeightPixels;
             int width = _imgView.Width;
 
             var bitmap = LoadAndResizeBitmap(_file.Path, width, height);
+
             _imgView.SetImageBitmap(bitmap);
         }
+
+        private string UniqueImgFileName() => $"img_{System.DateTime.Now.Ticks.ToString()}.jpg";
 
         public static Bitmap LoadAndResizeBitmap(string fileName, int width, int height)
         {
@@ -84,8 +94,6 @@ namespace CameraSharerer
             BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true };
             BitmapFactory.DecodeFile(fileName, options);
 
-            // Next we calculate the ratio that we need to resize the image by
-            // to fit the requested dimensions.
             int outHeight = options.OutHeight;
             int outWidth = options.OutWidth;
             int inSampleSize = 1;
@@ -97,14 +105,12 @@ namespace CameraSharerer
                                    : outWidth / width;
             }
 
-            // Now we will load the image and have BitmapFactory resize it for us.
             options.InSampleSize = inSampleSize;
             options.InJustDecodeBounds = false;
             Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
 
             return resizedBitmap;
         }
-
     }
 }
 
---
+++ CameraService/CameraSharerer/PermissionsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Support.V4.App;
+using Android.Support.V4.Content;
+
+
+namespace CameraSharerer.Managers
+{
+    public class PermissionsManager
+    {
+        private const int PERMISSIONS_REQUEST_CODE = 1001;
+        private string[] _permissions;
+        private Activity _activity;
+
+
+        public PermissionsManager(Activity currentActivity, string[] permissions)
+        {
+            _activity = currentActivity;
+            _permissions = permissions;
+        }
+
+        private bool IsPermissionEnabled(string name) => ContextCompat.CheckSelfPermission(_activity.ApplicationContext, name) != Permission.Granted;
+
+        public void EnablePermissions()
+        {
+            var neededPermissions = _permissions.Where(p => !IsPermissionEnabled(p)).ToArray();
+            ActivityCompat.RequestPermissions(_activity, neededPermissions, PERMISSIONS_REQUEST_CODE);
+        }
+    }
+}
